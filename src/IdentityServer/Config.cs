e841827ebbe51582f69c8f99b8943acd49af3c7b
rohitkvv <rohitkvv@gmail.com>
2020-04-11 17:06:50 +0200

    OpenidConnect authentication using mvc web app

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -10,9 +11,10 @@ namespace IdentityServer
     public static class Config
     {
         public static IEnumerable<IdentityResource> Ids =>
-            new IdentityResource[]
+            new List<IdentityResource>
             { 
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
 
         public static IEnumerable<ApiResource> Apis =>
@@ -24,6 +26,7 @@ namespace IdentityServer
         public static IEnumerable<Client> Clients =>
             new List<Client>
             {
+                // machine to machine client
                 new Client
                 {
                     ClientId = "Test Client",
@@ -33,6 +36,26 @@ namespace IdentityServer
                         new Secret("mysecret".Sha512())
                     },
                     AllowedScopes = { "testApi" }
+                },
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("mvcSecret".Sha512()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
         
